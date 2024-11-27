e8f1db176477218b1e266de1cc6d249fffb7718b
James David R. Macalaguing <jamesmacalaguing14@gmail.com>
2024-11-27 13:03:06 +0800

    new file:   Mobile/DynaHCare/eas.json

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -9,7 +9,7 @@ import DashboardExample from "./pages/DashboardPage"
 import Createform from "./pages/CreateformPage"
 import FormPage from "./pages/FormPage"
 
-import PrivateRoute from "./pages/PrivateRoute"
+import PrivateRoute from "./pages/ProtectedRoute"
 
 
 function App() {
